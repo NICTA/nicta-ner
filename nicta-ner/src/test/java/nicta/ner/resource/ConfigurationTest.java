6608f509912e8b7c53eaefe713d9d6b7d11b86fd
Mats Henrikson <mats@zakalwe.com>
2014-06-17 17:06:21 +1000

    Added a Configuration test.

---
+++ nicta-ner/src/test/java/nicta/ner/resource/ConfigurationTest.java
@@ -0,0 +1,97 @@
+/*
+ * #%L
+ * NICTA Named Entity Recogniser library
+ * %%
+ * Copyright (C) 2010 - 2014 NICTA
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+package nicta.ner.resource;
+
+import nicta.ner.classifier.feature.Feature;
+import nicta.ner.classifier.feature.FeatureMap;
+import nicta.ner.data.NameType;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static nicta.ner.classifier.feature.Feature.generateFeatureByName;
+import static org.testng.Assert.assertEquals;
+
+public class ConfigurationTest {
+
+    @DataProvider(name = "configTest")
+    public Object[][] configTestProvider() throws IOException {
+        //noinspection MagicNumber
+        return new Object[][]{
+                {Configuration.DEFAULT_CONFIG_RESOURCE,
+                 new Result(new FeatureMap(
+                         new ArrayList<Feature>() {{
+                             add(generateFeatureByName("RuledWordFeature", "PERSON_NAME"));
+                             add(generateFeatureByName("RuledWordFeature", "PERSON_KEYWORD"));
+                             add(generateFeatureByName("RuledWordFeature", "COUNTRY_NAME"));
+                             add(generateFeatureByName("RuledWordFeature", "CITY_NAME"));
+                             add(generateFeatureByName("RuledWordFeature", "ORG_KEYWORD"));
+                             add(generateFeatureByName("ExistingPhraseFeature", "WIKI_ORG"));
+                             add(generateFeatureByName("ExistingPhraseFeature", "WIKI_PERSON"));
+                             add(generateFeatureByName("ExistingPhraseFeature", "WIKI_PLACE"));
+                             add(generateFeatureByName("RuledWordFeature", "PLACE_KEYWORD"));
+                             add(generateFeatureByName("RuledWordFeature", "STATES"));
+                             add(generateFeatureByName("PrepositionContextFeature", "PREP_LOCATION"));
+                             add(generateFeatureByName("PrepositionContextFeature", "PREP_ORG"));
+                             add(generateFeatureByName("RuledWordFeature", "WIKI_ORG_EXTRACTION"));
+                             add(generateFeatureByName("RuledWordFeature", "WIKI_PER_EXTRACTION"));
+                             add(generateFeatureByName("RuledWordFeature", "WIKI_LOC_EXTRACTION"));
+                         }},
+                         new double[][]{
+                                 {0.0, 0.0, 15.0, 15.0, 0.0,
+                                  0.0, 0.0, 25.0, 20.0, 10.0,
+                                  10.0, 0.0, 0.0, 0.0, 10.0},
+                                 {20.0, 30.0, 0.0, 0.0, 0.0,
+                                  -10.0, 25.0, -10.0, 0.0, 0.0,
+                                  0.0, 0.0, -10.0, 10.0, -10.0},
+                                 {0.0, 0.0, 0.0, 0.0, 25.0,
+                                  25.0, -10.0, -10.0, 0.0, 0.0,
+                                  0.0, 10.0, 10.0, -10.0, -10.0}}),
+                            new ArrayList<NameType>() {{
+                                add(new NameType("LOCATION"));
+                                add(new NameType("PERSON"));
+                                add(new NameType("ORGANIZATION"));
+                            }}
+                 )},
+        };
+    }
+
+    @Test(dataProvider = "configTest")
+    public void configTest(final String configResource, final Result r) throws IOException {
+        final Configuration config = new Configuration(configResource);
+        assertEquals(config.getFeatureMap(), r.featureMap);
+        assertEquals(config.getNameTypes(), r.nameTypes);
+    }
+
+    private static class Result {
+        private final FeatureMap featureMap;
+        private final Collection<NameType> nameTypes;
+
+        private Result(final FeatureMap featureMap, final Collection<NameType> nameTypes) {
+            this.featureMap = featureMap;
+            this.nameTypes = nameTypes;
+        }
+    }
+}
