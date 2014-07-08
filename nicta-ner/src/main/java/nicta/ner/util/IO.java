cd57e9b3bfac181140f3f855d1c18ae878ec12e3
Mats Henrikson <mats@zakalwe.com>
2014-07-08 14:01:34 +1000

    Small refactorings.

--- nicta-ner/src/main/java/nicta/ner/util/IO.java
+++ nicta-ner/src/main/java/nicta/ner/util/IO.java
@@ -21,31 +21,32 @@
  */
 package nicta.ner.util;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.io.LineProcessor;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.List;
 import java.util.Set;
-import java.util.regex.Pattern;
 
+import static com.google.common.base.Charsets.UTF_8;
 import static com.google.common.io.Resources.getResource;
 import static com.google.common.io.Resources.readLines;
 
 public final class IO {
 
-    private static final Pattern SPACES = Pattern.compile(" ");
-    private static final Pattern TABS = Pattern.compile("\t");
-    private static final Charset UTF8 = Charset.forName("UTF-8");
+    private static final Splitter SPACES = Splitter.on(' ').trimResults().omitEmptyStrings();
+    private static final Splitter TABS = Splitter.on('\t').trimResults().omitEmptyStrings();
 
     private IO() {}
 
     /** Return a Set containing trimmed lines read from a file, skipping comments. */
     public static Set<String> lines(final Class<?> origin, final String resource) throws IOException {
-        return new HashSet<String>() {{
+        return ImmutableSet.copyOf(new HashSet<String>() {{
             readResource(origin, resource, new NullReturnLineProcessor() {
                 @Override
                 public boolean processLine(@Nonnull final String line) {
@@ -55,78 +56,79 @@ public final class IO {
                     return true;
                 }
             });
-        }};
+        }});
     }
 
     /** Returns a Set containing only single words. */
-    public static Set<String> createSingleWordSet(final Class<?> origin, final String resource,
-                                                  final boolean eliminatePrepAndConj) throws IOException {
-        return new HashSet<String>() {{
+    public static ImmutableSet<String> createSingleWordSet(final Class<?> origin, final String resource,
+                                                           final boolean eliminatePrepAndConj) throws IOException {
+        return ImmutableSet.copyOf(new HashSet<String>() {{
             readResource(origin, resource, new NullReturnLineProcessor() {
                 @Override
                 public boolean processLine(@Nonnull final String line) {
                     final String l = line.trim();
-                    if (!l.startsWith("#") && !l.isEmpty()) {
+                    if (!l.isEmpty() && !l.startsWith("#")) {
                         for (final String part : SPACES.split(l)) {
-                            if (!part.isEmpty()) {
-                                if (eliminatePrepAndConj) {
-                                    final String wordType = Dictionary.checkup(part);
-                                    if (wordType != null && (wordType.startsWith("IN") || wordType.startsWith("CC"))) {
-                                        continue;
-                                    }
+                            if (eliminatePrepAndConj) {
+                                final String wordType = Dictionary.checkup(part);
+                                if (wordType != null && (wordType.startsWith("IN") || wordType.startsWith("CC"))) {
+                                    continue;
                                 }
-                                // add to the containing HashSet we are currently in the init block of
-                                add(part);
                             }
+                            // add to the containing HashSet we are currently in the init block of
+                            add(part);
                         }
                     }
                     return true;
                 }
             });
-        }};
+        }});
     }
 
     /** Return a set containing all non-comment non-empty lower cased words. */
-    public static Set<String> lowerCasedWordSet(final Class<?> origin, final String resource) throws IOException {
-        return new HashSet<String>() {{
+    public static ImmutableSet<String> lowerCasedWordSet(final Class<?> origin, final String resource)
+            throws IOException {
+        return ImmutableSet.copyOf(new HashSet<String>() {{
             readResource(origin, resource, new NullReturnLineProcessor() {
                 @Override
                 public boolean processLine(@Nonnull final String line) {
                     final String l = line.trim();
                     // add to the containing HashSet we are currently in the init block of
-                    if (!l.startsWith("#") && !l.isEmpty()) add(l.toLowerCase());
+                    if (!l.isEmpty() && !l.startsWith("#")) add(l.toLowerCase());
                     return true;
                 }
             });
-        }};
+        }});
     }
 
-    /** Return a map/dictionary of words. */
-    public static Map<String, String> dictionary(final Class<?> origin, final String resource) throws IOException {
-        return new HashMap<String, String>() {{
+    /** Return a map/dictionary of words from a file of key/values separated by a tab character. */
+    public static ImmutableMap<String, String> dictionary(final Class<?> origin, final String resource)
+            throws IOException {
+        return ImmutableMap.copyOf(new HashMap<String, String>() {{
             readResource(origin, resource, new NullReturnLineProcessor() {
                 @Override
                 public boolean processLine(@Nonnull final String line) {
                     final String l = line.trim();
-                    if (!l.startsWith("#")) {
-                        final String[] parts = TABS.split(l);
-                        put(parts[0], parts[1]);
+                    if (!l.isEmpty() && !l.startsWith("#")) {
+                        final List<String> parts = TABS.splitToList(l);
+                        // add the parts to the map we are in the init block of
+                        put(parts.get(0), parts.get(1));
                     }
                     return true;
                 }
             });
-        }};
+        }});
     }
 
     private static <T> T readResource(final Class<?> origin, final String resource,
                                       final LineProcessor<T> processor) throws IOException {
-        try { return readLines(getResource(origin, resource), UTF8, processor); }
+        try { return readLines(getResource(origin, resource), UTF_8, processor); }
         catch (final IOException ioe) {
             throw new IOException("Error reading resource: '" + resource + "'", ioe);
         }
-
     }
 
+    // the way we are using the LineProcessor does not depend on the result that it returns
     private static abstract class NullReturnLineProcessor implements LineProcessor<Object> {
         @SuppressWarnings("ReturnOfNull")
         @Override
