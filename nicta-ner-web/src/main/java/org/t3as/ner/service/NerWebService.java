944474e525b50ff01fedd0ae76fc4bfc230c48f1
Mats Henrikson <mats@zakalwe.com>
2014-07-09 10:54:52 +1000

    Simplest implementation possible of the NER service.

--- nicta-ner-web/src/main/java/org/t3as/ner/service/NerWebService.java
+++ nicta-ner-web/src/main/java/org/t3as/ner/service/NerWebService.java
@@ -21,17 +21,43 @@
  */
 package org.t3as.ner.service;
 
+import org.t3as.ner.NamedEntityAnalyser;
+import org.t3as.ner.NerResultSet;
+import org.t3as.ner.resource.Configuration;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLDecoder;
 
 @Path("v1.0/ner")
 public class NerWebService {
+
+    static final Configuration conf;
+
+    static {
+        try { conf = new Configuration(); }
+        catch (final IOException e) { throw new RuntimeException("Could not load configuraton.", e); }
+    }
 
     @GET
     public InputStream getDoc() throws IOException {
         //noinspection ConstantConditions
         return getClass().getClassLoader().getResource("NerWebService_help.txt").openStream();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public NerResultSet ner(final String text) throws IOException {
+        final NamedEntityAnalyser nea = new NamedEntityAnalyser(conf);
+        final NerResultSet resultSet = nea.process(URLDecoder.decode(text, "UTF-8"));
+        System.out.println(resultSet);
+        return resultSet;
     }
 }
